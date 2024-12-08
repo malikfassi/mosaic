c90942a1ce8652a470faa6de84f56a3c93d3c06c
Malik Fassi <malikfassifihri@gmail.com>
2024-12-08 20:37:01 +0100

    fix: update Keplr window type with all required methods

--- frontend/src/types/test-utils.d.ts
+++ frontend/src/types/test-utils.d.ts
@@ -1,4 +1,6 @@
 import '@testing-library/jest-dom'
+import { OfflineSigner } from '@cosmjs/proto-signing'
+import { ChainInfo } from '@keplr-wallet/types'
 
 declare global {
   namespace jest {
@@ -14,6 +16,17 @@ declare global {
       getKey: jest.Mock<Promise<{
         bech32Address: string
         pubKey: Uint8Array
+      }>>
+      experimentalSuggestChain: jest.Mock<Promise<void>>
+      getOfflineSigner: jest.Mock<OfflineSigner>
+      getOfflineSignerOnlyAmino: jest.Mock<OfflineSigner>
+      getOfflineSignerAuto: jest.Mock<Promise<OfflineSigner>>
+      signArbitrary: jest.Mock<Promise<{
+        signature: Uint8Array
+        pub_key: {
+          type: string
+          value: string
+        }
       }>>
     }
   }
