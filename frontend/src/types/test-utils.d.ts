93dd9ac7eb050004fc4706b5d0ff4f23b539b365
Malik Fassi <malikfassifihri@gmail.com>
2024-12-08 20:46:26 +0100

    fix: update test utility types with proper mock signatures

--- frontend/src/types/test-utils.d.ts
+++ frontend/src/types/test-utils.d.ts
@@ -12,22 +12,22 @@ declare global {
 
   interface Window {
     keplr: {
-      enable: jest.Mock<Promise<void>>
+      enable: jest.Mock<Promise<void>, [string | string[]]>
       getKey: jest.Mock<Promise<{
         bech32Address: string
         pubKey: Uint8Array
-      }>>
-      experimentalSuggestChain: jest.Mock<Promise<void>>
-      getOfflineSigner: jest.Mock<OfflineSigner>
-      getOfflineSignerOnlyAmino: jest.Mock<OfflineSigner>
-      getOfflineSignerAuto: jest.Mock<Promise<OfflineSigner>>
+      }>, [string]>
+      experimentalSuggestChain: jest.Mock<Promise<void>, [ChainInfo]>
+      getOfflineSigner: jest.Mock<OfflineSigner, [string]>
+      getOfflineSignerOnlyAmino: jest.Mock<OfflineSigner, [string]>
+      getOfflineSignerAuto: jest.Mock<Promise<OfflineSigner>, [string]>
       signArbitrary: jest.Mock<Promise<{
         signature: Uint8Array
         pub_key: {
           type: string
           value: string
         }
-      }>>
+      }>, [string, string, string]>
     }
   }
 }
