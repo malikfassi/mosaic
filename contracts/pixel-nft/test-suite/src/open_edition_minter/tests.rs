9c16a07e0298a0b76ece133f1e73768b0e8d83de
Malik Fassi <malikfassifihri@gmail.com>
2024-12-09 11:49:59 +0100

    chore(deps): add Stargaze contract templates
    - Add sg721-base contract template\n- Add supporting contracts and tools\n- Add contract development infrastructure

---
+++ contracts/pixel-nft/test-suite/src/open_edition_minter/tests.rs
@@ -0,0 +1,9 @@
+mod address_limit;
+mod allowed_code_ids;
+mod complete_mint_all_outcomes_validation;
+mod factory_create_minter;
+mod frozen_factory;
+mod ibc_asset_mint;
+mod max_tokens_limit;
+mod update_mint_price;
+mod update_start_and_end_time;
